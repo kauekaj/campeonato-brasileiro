6f40cfa518bed910404bdd82458c3175185d6cb0
Kauê Jacyntho <kauekaj@gmail.com>
2022-03-14 16:06:17 -0300

    add navigation functionality

--- src/components/Shared/AppTopBar.vue
+++ src/components/Shared/AppTopBar.vue
@@ -18,19 +18,24 @@
           v-model="group"
           active-class="green--text text--lighten-1"
         >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Início</v-list-item-title>
-          </v-list-item>
+        <router-link to="/">
+            <v-list-item>
+                <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Início</v-list-item-title>
+            </v-list-item>
+        </router-link>
 
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-soccer</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Classificação</v-list-item-title>
-          </v-list-item>
+        <router-link to="/classificacao">
+            <v-list-item>
+                <v-list-item-icon>
+                <v-icon>mdi-soccer</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Classificação</v-list-item-title>
+            </v-list-item>
+         </router-link>
+
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -46,3 +51,9 @@ export default {
   }),
 }
 </script>
+
+<style scoped>
+    .app-top-bar a {
+        text-decoration: none;
+    }
+</style>
---
+++ src/views/ClubesLista.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+        <h2 class="text-h5 text-center mb-3 mt-5">
+            Tabela de Classificação
+        </h2>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'ClubesLista'   
+}
+</script> 
+
+<style scoped>
+
+</style>
