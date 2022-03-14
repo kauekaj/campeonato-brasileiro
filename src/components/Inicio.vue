df5c77ac0ea56c7b4de0e0e95bec0219df9f4e86
Kauê Jacyntho <kauekaj@gmail.com>
2022-03-14 15:09:57 -0300

    create menu bar + add text and image

---
+++ src/components/Inicio.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <h2 class="text-h5 text-center mb-3 mt-5">
+            Brasileiro Serie A
+        </h2>
+        <v-img src="https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.2974821:1596806515/Serie-A.png">
+        </v-img>
+    </v-container>
+
+</template>
+
+<script>
+export default {
+    name: 'Inicio'
+}
+</script>
+
+<style scoped>
+
+</style>
