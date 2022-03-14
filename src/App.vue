f992c1cf626c7ecc219d724badff195ade15c4a3
Kauê Jacyntho <kauekaj@gmail.com>
2022-03-14 15:44:25 -0300

    add router

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,20 @@
     <AppTopBar/>
 
     <v-main>
-        <Inicio/>
+        <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 
-import Inicio from './components/Inicio.vue'
 import AppTopBar from './components/Shared/AppTopBar'
 
 export default {
   name: 'App', 
   components: {
-    Inicio,
     AppTopBar
   }
 };
 </script>
+
