f5db086e92382d23dad2a8c1f9788ab24cfd041e
Kauê Jacyntho <kauekaj@gmail.com>
2022-03-14 16:45:42 -0300

    connecting api

--- src/views/ClubesLista.vue
+++ src/views/ClubesLista.vue
@@ -3,12 +3,57 @@
         <h2 class="text-h5 text-center mb-3 mt-5">
             Tabela de Classificação
         </h2>
+
+        <v-simple-table>
+            <template v-slot:default>
+            <thead>
+                <tr>
+                    <th colspan="2" class="text-left">
+                        Clubes
+                    </th>
+                    <th class="text-right">
+                        Pontos
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(clube, index) of clubeLista" :key="clube.id">    
+                    <td>{{ index + 1 }}</td>
+                    <td>
+                        <v-avatar size="24">
+                            <img
+                                :src="clube.escudo"
+                                :alt="clube.nome"
+                            >
+                        </v-avatar>
+                        <span class="pl-2">{{ clube.nome }}</span>
+                        
+                    </td>       
+                    <td class="text-right">{{ clube.pontos }}</td>
+                </tr>
+            </tbody>
+            </template>
+        </v-simple-table>
+
+
     </v-container>
 </template>
 
 <script>
 export default {
-    name: 'ClubesLista'   
+    name: 'ClubesLista',
+    data() {
+        return {
+            clubeLista: []
+        }
+    },
+    created() {
+        fetch('https://hackthon-decola.firebaseio.com/clubes-lista.json')
+        .then(resposta => resposta.json())
+        .then(json => {
+            this.clubeLista = json;
+        });
+    }
 }
 </script> 
 
